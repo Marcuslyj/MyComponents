6d78c6ee963b0976847481dcdace87c5a7d5120f
liyongjie <liyongjie@tvflnet.com>
2019-10-11 13:59:26 +0800

    测试

--- src/pages/secondSelectLeaf.vue
+++ src/pages/secondSelectLeaf.vue
@@ -24,6 +24,16 @@
             server:'https://dev-web-services.tvflnet.com/dimension/v1/1702001'
             }" >
         </transfer-selector>
+
+
+        <transfer-selector ref="test" :data="testData" :config="{
+            onlyLeaf: true,
+            expandTo: 1
+        }" @change="testChange"></transfer-selector>
+
+
+
+
     </div>
 
 
@@ -35,7 +45,9 @@ import transferSelector from "../components/second-select-levelControl/transfer-
 export default {
   name: "",
   data() {
-    return {};
+    return {
+        testData: null
+    };
   },
   components: {
     transferSelector
@@ -52,12 +64,86 @@ export default {
         this.$refs.tree4.initWithSelected({
             data: ["1", "2-1-50", "2-1-42"]
         });
+
+
+        this.testData = {
+            maxLevel: 3,
+            datas: [{
+                    id: "345B665BB10AD775C65DE0B6A9200B13",
+                    level: 1,
+                    name: "全部广告位",
+                    parentId: null,
+                    title: "全部广告位"
+                },
+                {
+                    id: 1,
+                    level: 3,
+                    name: "贴片广告",
+                    parentId: "5AF016C9D2B1E0BC505B041F2845B382",
+                    title: "贴片广告"
+                },
+                {
+                    id: 2,
+                    level: 3,
+                    name: "中插广告",
+                    parentId: "5AF016C9D2B1E0BC505B041F2845B382",
+                    title: "中插广告"
+                },
+                {
+                    id: 7,
+                    level: 3,
+                    name: "啦啦啦广告",
+                    parentId: "5AF016C9D2B1E0BC505B041F2845B382",
+                    title: "啦啦啦广告"
+                },
+                {
+                    id: "5AF016C9D2B1E0BC505B041F2845B382",
+                    level: 2,
+                    name: "腾讯",
+                    parentId: "345B665BB10AD775C65DE0B6A9200B13",
+                    title: "腾讯"
+                },
+                {
+                    id: 3,
+                    level: 3,
+                    name: "应用开屏",
+                    parentId: "7C09BC68CA2E11D7AE9403BE35D0A69E",
+                    title: "应用开屏"
+                },
+                {
+                    id: 8,
+                    level: 3,
+                    name: "东东",
+                    parentId: "7C09BC68CA2E11D7AE9403BE35D0A69E",
+                    title: "东东"
+                },
+                {
+                    id: "7C09BC68CA2E11D7AE9403BE35D0A69E",
+                    level: 2,
+                    name: "爱奇艺",
+                    parentId: "345B665BB10AD775C65DE0B6A9200B13",
+                    title: "爱奇艺"
+                }
+            ]
+        }
+
+        // setTimeout(()=>{
+            this.$nextTick(()=>{
+                this.$refs.test.setSelected({
+                    data: [1, 2, 7, 3, 8]
+                });
+            });
+
+        // },500);
       });
   },
   methods:{
     selectChange(data) {
       console.log("change了");
       console.log(data);
+    },
+    testChange(data){
+        console.log(data);
     }
   }
 };
